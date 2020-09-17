d507cd4fcc7d08ddc3ec5d1ed45d9d4d82e85a9b
unknown <singhtalwinder790@gmail.com>
2020-09-17 17:38:58 +0530

    Graph Traversal

--- Graph/Graph.js
+++ Graph/Graph.js
@@ -13,6 +13,37 @@ class Graph {
     }
     delete this.edges[vertex];
   }
+
+  traverseBFS(vertex, callback) {
+    let queue = [],
+      visited = {};
+    queue.push(vertex);
+    while (queue.length) {
+      vertex = queue.shift();
+      if (!visited[vertex]) {
+        visited[vertex] = true;
+        callback(vertex);
+        for (let adjacentVertex in this.edges[vertex]) {
+          queue.push(adjacentVertex);
+        }
+      }
+    }
+  }
+
+  traverseDFS(vertex, callback) {
+    let visited = {};
+    this._traverseDFS(vertex, visited, callback);
+  }
+
+  _traverseDFS(vertex, visited, callback) {
+    visited[vertex] = true;
+    callback(vertex);
+    for (let adjacentVertex in this.edges[vertex]) {
+      if (!visited[adjacentVertex]) {
+        this._traverseDFS(adjacentVertex, visited, callback);
+      }
+    }
+  }
 }
 
 module.exports = Graph;
